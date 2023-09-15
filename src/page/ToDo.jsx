a4026d161bd543730b921454036354e96c17a9ac
gkwk <124476558+gkwk@users.noreply.github.com>
2023-09-16 02:21:07 +0900

    todo 정리

--- src/page/ToDo.jsx
+++ src/page/ToDo.jsx
@@ -13,6 +13,8 @@ function ToDo() {
 
     const [ToDo_List_temptodo,set_ToDo_List_temptodo] = useState({})
 
+    const [ToDo_New,set_ToDo_New] = useState("")
+
     function NavActivator()  {
         document.getElementById('page_todo').className += " active";
     }
@@ -35,13 +37,12 @@ function ToDo() {
             })
             .then(response => {
                 if (response.status === 201) {
+                    console.log(response)
                     return response.json()
                 }
             })
             .then(response => {
                 set_ToDo_New("")
-
-                console.log(response)
 
                 const json_array = response
 
@@ -92,11 +93,9 @@ function ToDo() {
     function upateTodo(event) {
         event.preventDefault()
 
-        const TargetURL = "https://www.pre-onboarding-selection-task.shop/todos"
+        const TargetURL = "https://www.pre-onboarding-selection-task.shop/todos/"+event.target.getAttribute("comment_id")
         
         if (localStorage.getItem("access_token")){
-
-            // console.log(form_values.email)
 
             fetch(TargetURL, {
                 headers: {
@@ -105,12 +104,13 @@ function ToDo() {
                 },
                 method: 'put',
                 body: JSON.stringify({
-                    "todo": "",
-                    "isCompleted": true,
+                    "todo": ToDo_List_temptodo[event.target.getAttribute("comment_id")],
+                    "isCompleted": ToDo_List[event.target.getAttribute("comment_id")]["isCompleted"],
                     })
             })
             .then(response => {
                 if (response.status === 200) {
+                    console.log(response)
                     return response.json()
                 }
             })
@@ -148,14 +148,87 @@ function ToDo() {
                     set_ToDo_List(({...copy1}))
                     set_ToDo_List_idEditing(({...copy2}))
                     set_ToDo_List_temptodo(({...copy3}))
-
-                    console.log(response)
-
-                    console.log(copy1)
                 }
             })
         }
     }
+
+    function isCompletedChange(event){
+        set_ToDo_List({
+            ...ToDo_List,
+            [event.target.getAttribute("comment_id")]:{
+                ...ToDo_List[event.target.getAttribute("comment_id")],
+                "isCompleted": event.target.checked
+            }
+        })
+    }
+
+    function todoChange(event){
+        set_ToDo_List({
+            ...ToDo_List,
+            [event.target.getAttribute("comment_id")]:{
+                ...ToDo_List[event.target.getAttribute("comment_id")],
+                "todo": ToDo_List_temptodo[event.target.getAttribute("comment_id")]
+            }
+        })
+
+        upateTodo(event)
+    }
+
+    function temptodoChange(event){
+        set_ToDo_List_temptodo({
+            ...ToDo_List_temptodo,
+            [event.target.getAttribute("comment_id")]:event.target.value
+        })
+    }
+
+    function temptodoReset(event){
+        set_ToDo_List_temptodo({
+            ...ToDo_List_temptodo,
+            [event.target.getAttribute("comment_id")]:ToDo_List[event.target.getAttribute("comment_id")]["todo"]
+        })
+    }
+
+    function isEditingChange(event){
+        set_ToDo_List_idEditing({
+            ...ToDo_List_idEditing,
+            [event.target.getAttribute("comment_id")]:!(ToDo_List_idEditing[event.target.getAttribute("comment_id")])
+        })
+    }
+
+    function todoNewChange(event) {
+        set_ToDo_New(event.target.value)
+    }
+
+    function ToDolisting(ToDoList) {
+        return (
+            <div>
+                {Object.keys(ToDoList).map((id) => (
+                    <div key={id}>
+                        <li>
+                            <label>
+                                {ToDo_List_idEditing[id] ?
+                                    <div>
+                                        <input type="checkbox" comment_id={id} checked={ToDoList[id]["isCompleted"]} onChange={isCompletedChange}/>
+                                        <input data-testid="modify-input" comment_id={id} value={ToDo_List_temptodo[id]} onChange={temptodoChange} />
+                                        <button data-testid="submit-button" comment_id={id} onClick={(event) => {todoChange(event);isEditingChange(event)}}>제출</button>
+                                        <button data-testid="cancel-button" comment_id={id} onClick={(event) => {temptodoReset(event);isEditingChange(event)}}>취소</button>
+                                    </div>
+                                :
+                                    <div>
+                                        <input type="checkbox" comment_id={id} checked={ToDoList[id]["isCompleted"]} onChange={isCompletedChange}/>
+                                        <span>{ToDoList[id]["todo"]}</span>
+                                        <button data-testid="modify-button" comment_id={id} onClick={isEditingChange}>수정</button>
+                                        <button data-testid="delete-button" comment_id={id} onClick={deleteTodo}>삭제</button>
+                                    </div>
+                                }
+                            </label>
+                        </li>
+                    </div>
+                ))}
+            </div>
+        );
+    };
 
     useEffect(() => {
         if (!("access_token" in localStorage)) {
@@ -182,17 +255,6 @@ function ToDo() {
             console.log(response);
 
             const json_array = response;
-
-            // for (let index = 0; index < json_array.length; index++) {
-            //     const element = json_array[index];
-
-            //     set_ToDo_List(ToDo_List => ({
-            //         ...ToDo_List,
-            //         [element["id"]]:{
-            //             "todo" : element["todo"],
-            //             "isCompleted": element["isCompleted"]
-            //     }}))
-            // }
 
             json_array.forEach(element => {
                 set_ToDo_List(ToDo_List => ({
@@ -222,131 +284,12 @@ function ToDo() {
         console.log(ToDo_List_temptodo)
     },[ToDo_List,ToDo_List_idEditing,ToDo_List_temptodo])
 
-    function isCompletedChange(event){
-        set_ToDo_List({
-            ...ToDo_List,
-            [event.target.getAttribute("comment_id")]:{
-                ...ToDo_List[event.target.getAttribute("comment_id")],
-                "isCompleted": event.target.checked
-            }
-        })
-    }
-
-    function todoChange(event){
-        set_ToDo_List({
-            ...ToDo_List,
-            [event.target.getAttribute("comment_id")]:{
-                ...ToDo_List[event.target.getAttribute("comment_id")],
-                "todo": ToDo_List_temptodo[event.target.getAttribute("comment_id")]
-            }
-        })
-    }
-
-    function temptodoChange(event){
-        set_ToDo_List_temptodo({
-            ...ToDo_List_temptodo,
-            [event.target.getAttribute("comment_id")]:event.target.value
-        })
-    }
-
-    function temptodoReset(event){
-        set_ToDo_List_temptodo({
-            ...ToDo_List_temptodo,
-            [event.target.getAttribute("comment_id")]:ToDo_List[event.target.getAttribute("comment_id")]["todo"]
-        })
-    }
-
-    function isEditingChange(event){
-        set_ToDo_List_idEditing({
-            ...ToDo_List_idEditing,
-            [event.target.getAttribute("comment_id")]:!(ToDo_List_idEditing[event.target.getAttribute("comment_id")])
-        })
-    }
-
-
-    const [ToDo_New,set_ToDo_New] = useState("")
-
-    function todoNewChange(event) {
-        set_ToDo_New(event.target.value)
-    }
-
-
-
-    function ToDolisting(ToDoList) {
-        
-    
-
-        return (
-            <div>
-                {/* <div className={ExampleCondition ? "" : undefined}>text</div> */}
-                {Object.keys(ToDoList).map((id) => (
-                    <div key={id}>
-                        <li>
-                            <label>
-                                {/* <input type="checkbox" comment_id={id} checked={ToDoList[id]["isCompleted"]} onChange={isCompletedChange}/> */}
-                                
-                                
-                                {ToDo_List_idEditing[id] ?
-                                    <div>
-                                        <input type="checkbox" comment_id={id} checked={ToDoList[id]["isCompleted"]} onChange={isCompletedChange}/>
-                                        <input data-testid="modify-input" comment_id={id} value={ToDo_List_temptodo[id]} onChange={temptodoChange} />
-                                        <button data-testid="submit-button" comment_id={id} onClick={(event) => {todoChange(event);isEditingChange(event)}}>제출</button>
-                                        <button data-testid="cancel-button" comment_id={id} onClick={(event) => {temptodoReset(event);isEditingChange(event)}}>취소</button>
-                                    </div>
-
-                                :
-                                    <div>
-                                        <input type="checkbox" comment_id={id} checked={ToDoList[id]["isCompleted"]} onChange={isCompletedChange}/>
-                                        <span>{ToDoList[id]["todo"]}</span>
-                                        <button data-testid="modify-button" comment_id={id} onClick={isEditingChange}>수정</button>
-                                        <button data-testid="delete-button" comment_id={id} onClick={deleteTodo}>삭제</button>
-                                    </div>
-                                }
-                            </label>
-
-                            {/* <button data-testid="modify-button">수정</button>
-                            <button data-testid="delete-button">삭제</button>
-
-                            <input data-testid="modify-input" />
-                            <button data-testid="submit-button">제출</button>
-                            <button data-testid="cancel-button">취소</button> */}
-                        </li>
-                    </div>
-                ))}
-            </div>
-        );
-    };
 
     return (
         <div className="d-flex flex-column h-100">
             { Header() }
 
             { ToDolisting(ToDo_List) }
-
-            {/* <li>
-                <label>
-                    <input type="checkbox" />
-                    <span>TODO 1</span>
-                </label>
-                <button data-testid="modify-button">수정</button>
-                <button data-testid="delete-button">삭제</button>
-
-                <input data-testid="modify-input" />
-                <button data-testid="submit-button">제출</button>
-                <button data-testid="cancel-button">취소</button>
-            </li>
-            <li>
-                <label>
-                    <input type="checkbox" />
-                    <span>TODO 2</span>
-                </label>
-                <button data-testid="modify-button">수정</button>
-                <button data-testid="delete-button">삭제</button>
-                
-                <input data-testid="modify-input" />
-                <button data-testid="submit-button">제출</button>
-                <button data-testid="cancel-button">취소</button>
-            </li> */}
 
             <input data-testid="new-todo-input" value={ToDo_New} onChange={todoNewChange} />
             <button data-testid="new-todo-add-button" onClick={createTodo}>추가</button>
@@ -355,43 +298,6 @@ function ToDo() {
             { Footer() } 
         </div>
     );
-
-    // return (
-    //     <div className="d-flex flex-column h-100">
-    //         { Header() }
-
-    //         <li>
-    //             <label>
-    //                 <input type="checkbox" />
-    //                 <span>TODO 1</span>
-    //             </label>
-    //             <button data-testid="modify-button">수정</button>
-    //             <button data-testid="delete-button">삭제</button>
-
-    //             <input data-testid="modify-input" />
-    //             <button data-testid="submit-button">제출</button>
-    //             <button data-testid="cancel-button">취소</button>
-    //         </li>
-    //         <li>
-    //             <label>
-    //                 <input type="checkbox" />
-    //                 <span>TODO 2</span>
-    //             </label>
-    //             <button data-testid="modify-button">수정</button>
-    //             <button data-testid="delete-button">삭제</button>
-                
-    //             <input data-testid="modify-input" />
-    //             <button data-testid="submit-button">제출</button>
-    //             <button data-testid="cancel-button">취소</button>
-    //         </li>
-
-    //         <input data-testid="new-todo-input" />
-    //         <button data-testid="new-todo-add-button">추가</button>
-
-
-    //         { Footer() } 
-    //     </div>
-    // );
 };
 
 export default ToDo;
